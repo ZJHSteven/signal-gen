d12423e36d6f8f533b685af0ff0acd183eea05af
Steven ZJH <zhangjiahe0830@gmail.com>
2025-05-13 18:54:45 +0800

    添加 MCP_POT 库支持，更新 my_ad9833.cpp 中的电位器实例化和初始化代码

--- src/my_ad9833.cpp
+++ src/my_ad9833.cpp
@@ -1,13 +1,19 @@
 #include "my_ad9833.h"
 #include <AD9833.h>
+#include <MCP_POT.h>
 #include "state.h" // 访问 systemState 和 lastState
 
 #define FSYNC 5 // FSYNC 接 GPIO5（你可改）
+#define CS_POT 4 // MCP41010 片选
 
 AD9833 gen(FSYNC);
+// 实例化 MCP41010
+MCP41010 pot(CS_POT, 255, 255);
 
 void initAD9833()
 {
+    SPI.begin();  // 注意：必须先初始化 SPI
+    pot.begin(128); // 将电位器初始化为最大值（最大增益）
     gen.Begin();
     gen.ApplySignal(SINE_WAVE, REG0, systemState.sineFreq);
     gen.EnableOutput(true);
