134e59cdbdf40896fa156e54dc767354ae3ace44
Steven ZJH <zhangjiahe0830@gmail.com>
2025-05-14 12:20:35 +0800

    修改 PWM 参数配置，使用 systemState 中的 pwmFreq 和 pwmDuty 代替硬编码值，并添加更新函数以动态调整 PWM 设置

--- src/pwm.cpp
+++ src/pwm.cpp
@@ -1,11 +1,12 @@
 #include "pwm.h"
+#include "state.h"
 
 // 配置 PWM 输出的相关参数
 const int pwmPin = 19; // PWM 输出引脚
 const int pwmChannel = 0; // 使用的PWM通道
-const int pwmFreq = 1000;    // 频率 1kHz
+const int pwmFreq = systemState.pwmFreq;    // 频率 1kHz
 const int pwmResolution = 8; // 8位分辨率 → 占空比范围0~255
-const int pwmDuty = 128;     // 占空比50%
+const int pwmDuty = systemState.pwmDuty;     // 占空比50%
 
 // 初始化 PWM
 void initPWM() {
@@ -14,3 +15,20 @@ void initPWM() {
     ledcWrite(pwmChannel, pwmDuty);                // 设置PWM占空比，开始输出
 }
 
+// 更新 PWM 参数
+void updatePWMWithCheck()
+{
+    // 更新 PWM 频率（仅在频率发生变化时更新）
+    if (systemState.pwmFreq != lastState.pwmFreq)
+    {
+        ledcSetup(pwmChannel, systemState.pwmFreq, pwmResolution);
+        lastState.pwmFreq = systemState.pwmFreq; // 更新 lastState
+    }
+
+    // 更新 PWM 占空比（仅在占空比发生变化时更新）
+    if (systemState.pwmDuty != lastState.pwmDuty)
+    {
+        ledcWrite(pwmChannel, systemState.pwmDuty);
+        lastState.pwmDuty = systemState.pwmDuty; // 更新 lastState
+    }
+}
